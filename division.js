32d058b4ce44030d5abeeab37802895b4af690a2
Anjum <sanjum77@gmail.com>
2022-12-09 03:13:45 +0500

    Added the divided function

---
+++ division.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function divide(a, b) {
+    return a / b;
+}
+console.log(divide(45, 5));
---
+++ division.ts
@@ -0,0 +1,6 @@
+function divide(a:number,b:number)
+{
+    return a/b
+}
+
+console.log(divide(45,5))
